da12aa649bbe347ad357d7365341b234625fdc60
Ian321 <ignaz.k@live.de>
2016-06-28 17:29:10 +0200

    Update mods.php

--- commands/mods.php
+++ commands/mods.php
@@ -1,6 +1,6 @@
 <?php
 	if (strpos($data, $admin."@".$admin.".tmi.twitch.tv PRIVMSG ".$channel.' :!mods') !== false) {
-		echo "=> MODS\n"
+		echo "=> MODS\n";
 		echo var_dump($mods);
 		if ($showS) {
 			fwrite($sock, "PRIVMSG ".$channel." :List of mods: ".implode(", ", $mods)."\n");
