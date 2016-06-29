001fcbad1e006c3eb248bb3e57cf95ce6e48fe4e
Ian321 <ignaz.k@live.de>
2016-06-29 22:52:32 +0200

    Add files via upload

---
+++ commands/hug.php
@@ -0,0 +1,21 @@
+<?php
+	if (!isset($C_kill_t)) {
+		$C_hug_t = 0;
+		$LastHug = null;
+	}
+	
+	$C_hug_n = time() - $C_hug_t;
+	if (checkC("all", "!hug") && ($C_hug_t >= 30 || $LastHug != $MSfrom."-".$varsIN[1])) {
+		if (strpos($varsIN[1], '.') !== false) {
+			fwrite($sock, "PRIVMSG ".$channel." :".$MSfrom.", you can't hug links OMGScoots\n");
+		} elseif (strlen($varsIN[1]) > 25) {
+			fwrite($sock, "PRIVMSG ".$channel." :".$MSfrom.", this string is too long OMGScoots\n");
+		} else {
+			echo "=> !hug ".$MSfrom." -> ".$varsIN[1]."\n";
+			fwrite($sock, "PRIVMSG ".$channel." :".$MSfrom." hugs ".$varsIN[1]." <3 VoHiYo\n");
+			$LastHug = $MSfrom."-".$varsIN[1];
+			$C_hug_t = time();
+		}
+
+	}
+?>
