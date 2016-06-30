b8062bc4ebf3bc311d26305326b20c2f33e1c09f
Ian321 <ignaz.k@live.de>
2016-06-30 18:59:22 +0200

    Update ping.php

--- commands/ping.php
+++ commands/ping.php
@@ -5,10 +5,42 @@
 	$C_ping_n = time() - $C_ping_t;
 	
 	$uptime = time() - $startTime;
+	$hours = floor($uptime / 3600);
+	$mins = floor($uptime / 60 % 60);
+	$secs = floor($uptime % 60);
+	$uptimeA = "";
+	if ($hours >= 2) {
+		$uptimeA .= $hours." hours";
+	} elseif ($hours >= 1) {
+		$uptimeA .= $hours." hour";
+	}
+	if ($hours >= 1 && $mins >= 1 && $secs >= 1) {
+		$uptimeA .= ", ";
+	} elseif ($hours >= 1 && ($mins >= 1 || $secs >= 1)) {
+		$uptimeA .= " and ";
+	} else {
+		$uptimeA .= ".";
+	}
+	if ($mins >= 2) {
+		$uptimeA .= $mins." minutes";
+	} elseif ($mins >= 1) {
+		$uptimeA .= $mins." minute";
+	}
+	if ($mins >= 1 && $secs >= 1) {
+		$uptimeA .= " and ";
+	} else {
+		$uptimeA .= ".";
+	}
+	if ($secs >= 2) {
+		$uptimeA .= $secs." seconds.";
+	} elseif ($secs >= 1) {
+		$uptimeA .= $secs." second.";
+	}
+
 	if (checkC("all", "!ping")) {
 		echo "==> !ping :".$uptime." (".$C_ping_n.")\n";
 		if ($C_ping_n >= 15) {
-			fwrite($sock, "PRIVMSG ".$channel." :Uptime is seconds: ".$uptime."\n");
+			fwrite($sock, "PRIVMSG ".$channel." :running for ".$uptimeA."\n");
 			$C_ping_t = time();
 		}
 	}
